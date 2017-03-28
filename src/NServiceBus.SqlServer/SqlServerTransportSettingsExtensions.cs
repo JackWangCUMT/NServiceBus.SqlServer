d091565272e9129c7be20596d39e1f9982ea46c7
SzymonPobiega <szymon.pobiega@gmail.com>
2017-03-28 10:35:49 +0200

    Added more (imperial) guards.

--- src/NServiceBus.SqlServer/SqlServerTransportSettingsExtensions.cs
+++ src/NServiceBus.SqlServer/SqlServerTransportSettingsExtensions.cs
@@ -16,6 +16,7 @@
         /// </summary>
         public static TransportExtensions<SqlServerTransport> DefaultSchema(this TransportExtensions<SqlServerTransport> transportExtensions, string schemaName)
         {
+            Guard.AgainstNull(nameof(transportExtensions), transportExtensions);
             Guard.AgainstNullAndEmpty(nameof(schemaName), schemaName);
 
             transportExtensions.GetSettings().Set(SettingsKeys.DefaultSchemaSettingsKey, schemaName);
@@ -32,6 +33,7 @@
         /// <returns></returns>
         public static TransportExtensions<SqlServerTransport> UseSchemaForEndpoint(this TransportExtensions<SqlServerTransport> transportExtensions, string endpointName, string schema)
         {
+            Guard.AgainstNull(nameof(transportExtensions), transportExtensions);
             Guard.AgainstNullAndEmpty(nameof(endpointName), endpointName);
             Guard.AgainstNullAndEmpty(nameof(schema), schema);
 
@@ -51,6 +53,7 @@
         /// <returns></returns>
         public static TransportExtensions<SqlServerTransport> UseSchemaForQueue(this TransportExtensions<SqlServerTransport> transportExtensions, string queueName, string schema)
         {
+            Guard.AgainstNull(nameof(transportExtensions), transportExtensions);
             Guard.AgainstNullAndEmpty(nameof(queueName), queueName);
             Guard.AgainstNullAndEmpty(nameof(schema), schema);
 
@@ -70,6 +73,7 @@
         /// <returns></returns>
         public static TransportExtensions<SqlServerTransport> UseCatalogForEndpoint(this TransportExtensions<SqlServerTransport> transportExtensions, string endpointName, string catalog)
         {
+            Guard.AgainstNull(nameof(transportExtensions), transportExtensions);
             Guard.AgainstNullAndEmpty(nameof(endpointName), endpointName);
             Guard.AgainstNullAndEmpty(nameof(catalog), catalog);
 
@@ -89,6 +93,7 @@
         /// <returns></returns>
         public static TransportExtensions<SqlServerTransport> UseCatalogForQueue(this TransportExtensions<SqlServerTransport> transportExtensions, string queueName, string catalog)
         {
+            Guard.AgainstNull(nameof(transportExtensions), transportExtensions);
             Guard.AgainstNullAndEmpty(nameof(queueName), queueName);
             Guard.AgainstNullAndEmpty(nameof(catalog), catalog);
 
@@ -108,6 +113,9 @@
         /// <param name="waitTime">Time to wait before triggering the circuit breaker.</param>
         public static TransportExtensions<SqlServerTransport> TimeToWaitBeforeTriggeringCircuitBreaker(this TransportExtensions<SqlServerTransport> transportExtensions, TimeSpan waitTime)
         {
+            Guard.AgainstNull(nameof(transportExtensions), transportExtensions);
+            Guard.AgainstNegativeAndZero(nameof(waitTime), waitTime);
+
             transportExtensions.GetSettings().Set(SettingsKeys.TimeToWaitBeforeTriggering, waitTime);
             return transportExtensions;
         }
@@ -119,6 +127,7 @@
         /// <param name="sqlConnectionFactory">Factory that returns connection ready for usage.</param>
         public static TransportExtensions<SqlServerTransport> UseCustomSqlConnectionFactory(this TransportExtensions<SqlServerTransport> transportExtensions, Func<Task<SqlConnection>> sqlConnectionFactory)
         {
+            Guard.AgainstNull(nameof(transportExtensions), transportExtensions);
             Guard.AgainstNull(nameof(sqlConnectionFactory), sqlConnectionFactory);
 
             transportExtensions.GetSettings().Set(SettingsKeys.ConnectionFactoryOverride, sqlConnectionFactory);
@@ -135,6 +144,8 @@
         /// </remarks>
         public static TransportExtensions<SqlServerTransport> TransactionScopeOptions(this TransportExtensions<SqlServerTransport> transportExtensions, TimeSpan? timeout = null, IsolationLevel? isolationLevel = null)
         {
+            Guard.AgainstNull(nameof(transportExtensions), transportExtensions);
+
             transportExtensions.GetSettings().Set<SqlScopeOptions>(new SqlScopeOptions(timeout, isolationLevel));
             return transportExtensions;
         }
@@ -147,6 +158,8 @@
         /// <returns></returns>
         public static TransportExtensions<SqlServerTransport> WithPeekDelay(this TransportExtensions<SqlServerTransport> transportExtensions, TimeSpan? delay = null)
         {
+            Guard.AgainstNull(nameof(transportExtensions), transportExtensions);
+
             transportExtensions.GetSettings().Set<QueuePeekerOptions>(new QueuePeekerOptions(delay));
             return transportExtensions;
         }
@@ -159,6 +172,7 @@
         [ObsoleteEx(RemoveInVersion = "4.0", TreatAsErrorFromVersion = "4.0", Message = "Multi-instance mode has been deprecated and is no longer a recommended model of deployment. Please refer to documentation for more details.")]
         public static TransportExtensions<SqlServerTransport> EnableLegacyMultiInstanceMode(this TransportExtensions<SqlServerTransport> transportExtensions, Func<string, Task<SqlConnection>> sqlConnectionFactory)
         {
+            Guard.AgainstNull(nameof(transportExtensions), transportExtensions);
             Guard.AgainstNull(nameof(sqlConnectionFactory), sqlConnectionFactory);
 
             transportExtensions.GetSettings().Set(SettingsKeys.LegacyMultiInstanceConnectionFactory, sqlConnectionFactory);
